8dc18effd56e94b53d1dc86015689065192ca262
Varun Verma <varun.ver@gmail.com>
2014-04-14 22:36:58 +1000

    minor refactor

---
+++ src/main/java/com/demo/swagger/controller/BookController.java
@@ -0,0 +1,41 @@
+package com.demo.swagger.controller;
+
+import com.demo.swagger.dao.BookDao;
+import com.demo.swagger.model.Book;
+import com.wordnik.swagger.annotations.ApiError;
+import com.wordnik.swagger.annotations.ApiErrors;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    BookDao bookDao;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ApiOperation(value = "Retrieves a book based on their id")
+    @ApiErrors(value = {@ApiError(code=404, reason = "No book corresponding to the id was found")})
+    @ResponseBody
+    public ResponseEntity<String> showJson(@ApiParam(name = "id" , required = true, value = "The id of the book that needs to be retrieved")@PathVariable("id") Long id) {
+        Book book = bookDao.findBook(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        if (book == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<String>(book.toJson(), headers, HttpStatus.OK);
+    }
+
+
+}
---
+++ src/main/java/com/demo/swagger/dao/BookDao.java
@@ -0,0 +1,31 @@
+package com.demo.swagger.dao;
+
+import com.demo.swagger.model.Book;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * Created by varunverma on 11/04/2014.
+ */
+@Repository
+public class BookDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public static final List<String> fieldNames4OrderClauseFilter = java.util.Arrays.asList("");
+
+    public final EntityManager entityManager() {
+        EntityManager em = entityManager;
+        if (em == null) throw new IllegalStateException("Entity manager has not been injected (is the Spring Aspects JAR configured as an AJC/AJDT aspects library?)");
+        return em;
+    }
+
+    public Book findBook(Long id) {
+        if (id == null) return null;
+        return entityManager().find(Book.class, id);
+    }
+}
